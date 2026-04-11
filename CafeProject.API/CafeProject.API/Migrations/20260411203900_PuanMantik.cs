cd38c7356497549fc97ea7c1e4432775486ad9b2
yakuphanusluu <yakuphan16@hotmail.com>
2026-04-11 23:41:01 +0300

    Değişiklik yapıldı.

---
+++ CafeProject.API/CafeProject.API/Migrations/20260411203900_PuanMantik.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeProject.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PuanMantik : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UsedPoints",
+                table: "Orders",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsedPoints",
+                table: "Orders");
+        }
+    }
+}
